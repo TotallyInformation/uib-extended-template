f96c96ddf28f3a20b376e77e421b7f42f747a876
Julian Knight <1591850+TotallyInformation@users.noreply.github.com>
2025-05-18 19:51:27 +0100

    Enhancement 2

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 /** Extended example of using uibuilder
  */
 
+/** Toggle visible messages from Node-RED
+ * NB: If using this with ES Modules (ESM) then you may need to attach
+ *     the function to the window object. `window.toggleMsgs = function toggleMsgs() { ... }`
+ */
 function toggleMsgs() {
     uibuilder.showMsg()
 }
 
 // Listen for incoming messages from Node-RED and action
 uibuilder.onChange('msg', (msg) => {
-    // do stuff with the incoming msg
+    console.log('Incoming message from Node-RED:', msg)
 })
 
 // Alternatively, listen for messages with a specific topic
 uibuilder.onTopic('myTopic', (msg) => {
-    // do stuff with the incoming msg
+    console.log('Incoming message with topic "myTopic":', msg)
 })
 
-// Send a custom message from the browser to the Node-RED server
+// Send a custom message from the browser to the Node-RED server when the page loads
 uibuilder.send({
     // Content can be any JSON but it is often best to follow the standard
     // Node-RED message structure.
